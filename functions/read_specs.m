55437ce879bc7eb18e299f8535f03209cef5ebf8
amal-araweelo <amal-araweelo@github.com>
2025-11-05 19:08:16 +0100

    1 extra function template + sectioning of main script

--- functions/read_specs.m
+++ functions/read_specs.m
@@ -1,5 +1,5 @@
 function [outputArg1,outputArg2] = read_specs(inputArg1,inputArg2)
-%READ_SPECS Summary of this function goes here
+%READ_SPECS Read and parse global_specs.txt
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
